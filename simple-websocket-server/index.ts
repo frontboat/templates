387c2e545c2ee19f38db15336706c004b4956842
frontboat <156139764+frontboat@users.noreply.github.com>
2025-12-25 21:39:20 -0700

    init boat templates

---
+++ simple-websocket-server/index.ts
@@ -0,0 +1,26 @@
+const server = Bun.serve({
+    fetch(req, server) {
+      const success = server.upgrade(req);
+      if (success) {
+        // Bun automatically returns a 101 Switching Protocols
+        // if the upgrade succeeds
+        return undefined;
+      }
+  
+      // handle HTTP request normally
+      return new Response("Hello world!");
+    },
+    websocket: {
+      // TypeScript: specify the type of ws.data like this
+      data: {} as { authToken: string },
+  
+      // this is called when a message is received
+      async message(ws, message) {
+        console.log(`Received ${message}`);
+        // send back a message
+        ws.send(`You said: ${message}`);
+      },
+    },
+  });
+  
+  console.log(`Listening on ${server.hostname}:${server.port}`);
